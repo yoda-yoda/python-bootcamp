d59dd178eeb24d77b723adebeffbeba6076d5bee
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-11-24 22:14:36 +0300

    Moved python bootcamp files in 'beginner-files' dir

--- function.py
+++
@@ -1,6 +0,0 @@
-def getSquare(numSquared):
-	return numSquared * numSquared
-	#end of method()
-	
-number = int(raw_input('Enter a number to get it\'s square\n'))
-print 'The square of',number,'is', getSquare(number)
